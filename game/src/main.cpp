6e6e02563c9c6a7ec7f04a4e4fd228c74fbddc9e
Enlighten0068 <guimatos97@gmail.com>
2026-07-29 23:39:08 +0100

    SDL3 runtime initialization

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,3 +1,16 @@
+#include <SDL3/SDL.h>
+#include <iostream>
+
+
 int main(){
+  std::cout << "Teste de inicialização do SDL3.\n";
+  if (SDL_Init(SDL_INIT_VIDEO) < 0 ){
+    std::cout << "Erro na inicialização: " << SDL_GetError() << '\n';
+    return 1;
+  }
+  else{
+    std::cout << "Sucesso.\n";
+  }
+  SDL_Quit();
   return 0;
 }
