491b51b24d9625760820b18d4ae0025dbf68e712
Enlighten0068 <guimatos97@gmail.com>
2026-07-30 12:41:44 +0100

    OpenGL implementation

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -15,7 +15,20 @@ int main(){
 
   if (videoDriver) std::cout << "Video Driver: " << videoDriver << '\n';
 
-  SDL_Window* window = SDL_CreateWindow("Game Window", 1920, 1080, SDL_WINDOW_RESIZABLE);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+  SDL_Window* window = SDL_CreateWindow("Game Window", 1920, 1080, SDL_WINDOW_OPENGL);
+  SDL_GLContext glContext = SDL_GL_CreateContext(window);
+  //std::cout << "OpenGL Version: " << SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION) << '\n';
+
+  if(!glContext){
+    std::cerr << "OpenGL context not created: " << SDL_GetError() << '\n';
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
 
   if (!window){
     std::cerr << "Window creation error: " << SDL_GetError() << '\n';
@@ -26,6 +39,10 @@ int main(){
   std::cout << "Window successfully created.\n";
   
   SDL_Delay(10000);
+
+  SDL_GL_DestroyContext(glContext);
+  std::cout << "OpenGL terminated.\n";
+
   SDL_DestroyWindow(window);
   std::cout << "Window disposal success.\n";
 
